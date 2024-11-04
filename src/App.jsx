38181f70d0286a4bf78ef9193a63132596603e38
tahmina-nishu <akthertahmina572@gmail.com>
2024-11-04 11:29:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+
+function App() {
+
+  return (
+    <>
+    <DaisyNav></DaisyNav>
+      <h1 className='text-7xl flex justify-center border-2 border-orange-600'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
